a2defa6cd2dca016e613759bc43c066d020b2e74
Your Name <akibh805@gmail.com>
2020-06-21 05:13:03 +0600

    Mejor update in all pages

--- js/main.js
+++ js/main.js
@@ -1,5 +1,45 @@
 
 $(document).ready(function () {
+
+   // Hide Header on on scroll down
+   var didScroll;
+   var lastScrollTop = 0;
+   var delta = 5;
+   var navbarHeight = $('.header').outerHeight();
+
+   $(window).scroll(function (event) {
+      didScroll = true;
+   });
+
+   setInterval(function () {
+      if (didScroll) {
+         hasScrolled();
+         didScroll = false;
+      }
+   }, 100);
+
+   function hasScrolled() {
+      var st = $(this).scrollTop();
+
+      // Make sure they scroll more than delta
+      if (Math.abs(lastScrollTop - st) <= delta)
+         return;
+
+      // If they scrolled down and are past the navbar, add class .nav-up.
+      // This is necessary so you never see what is "behind" the navbar.
+      if (st > lastScrollTop && st > navbarHeight) {
+         // Scroll Down
+         $('.header').removeClass('nav-down').addClass('nav-up');
+      } else {
+         // Scroll Up
+         if (st + $(window).height() < $(document).height()) {
+            $('.header').removeClass('nav-up').addClass('nav-down');
+         }
+      }
+
+      lastScrollTop = st;
+   }
+
    // Menu Toggle 
    $(".menu-btn").click(function () {
       $(".menu-list").toggleClass('added');
@@ -17,7 +57,7 @@ $(document).ready(function () {
    // Box click and Hide that and Showing Slider
    $('.box').click(function () {
       $('.optimus-do').hide();
-      $('#slider').css('display', 'block');
+      $('.slider-area').addClass('showSlider')
    })
 
 
